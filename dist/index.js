4d06ccabb44d5c5bae23d7902bc54dacdaa55af3
Sahariar Kabir <sronok@gmail.com>
2023-01-05 00:09:42 +0600

    fundamentals

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log('Hello World!');
+let age = 29;
+if (age < 50)
+    age += 10;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 console.log('Hello World!');
+let age:number = 29;
+
+if(age < 50)
+    age +=10
