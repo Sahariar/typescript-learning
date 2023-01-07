615475d1d03ac3b21a3fc164ee1d0c041ab4491e
Sahariar Kabir <sronok@gmail.com>
2023-01-07 08:47:57 +0600

    type alias

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
-console.log('Hello World!');
-let age:number = 29;
+console.log("Hello World!");
+let age: number = 29;
 
-if(age < 50)
-    age +=10
+if (age < 50) age += 10;
 
-    // functions
+// functions
 
-    function calculateTax (income:number , taxYear = 2022):number {
-            if(taxYear > 2022)
-            return income * 1.2
-                return income * 1.3
-            }
+function calculateTax(income: number, taxYear = 2022): number {
+	if (taxYear > 2022) return income * 1.2;
+	return income * 1.3;
+}
 
 console.log(calculateTax(40000));
 
-
 // objects in TypeScript
+type Employee = {
+	readonly id: number;
+	name: String;
+	retire: (date: Date) => void;
+};
 
-let employee : {
-    readonly id: number,
-    name:String,
-    retire : (date : Date) => void
-} = {id: 1 , name:"Sahariar" , retire:(date: Date)=>{
-    console.log(date);
-}}
-
-
-
+let employee: Employee = {
+	id: 1,
+	name: "Sahariar",
+	retire: (date: Date) => {
+		console.log(date);
+	},
+};
