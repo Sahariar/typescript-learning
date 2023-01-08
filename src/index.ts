2e332e82de7d457bc71fc44fe6a6acadffb2746e
Sahariar Kabir <sronok@gmail.com>
2023-01-08 23:53:45 +0600

    intersection Type In Ts

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,19 @@ function kgToLbs(weight: number | string): number {
 
 kgToLbs(10);
 kgToLbs("30");
+
+// intersection
+
+type Draggable = {
+	drag: () => void;
+};
+type Resizable = {
+	resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+	drag: () => {},
+	resize: () => {},
+};
